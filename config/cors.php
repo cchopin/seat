4b85f85eb3c377e0902dc1b1f28e9d0ec6818d3c
clemence <cchopin@altima-assurances.fr>
2026-05-11 20:17:26 +0200

    fix: restrict CORS policy to application origin

--- config/cors.php
+++ config/cors.php
@@ -17,13 +17,13 @@ return [
 
     'paths' => ['api/*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [env('APP_URL', 'http://localhost')],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'X-Token'],
 
     'exposed_headers' => [],
 
